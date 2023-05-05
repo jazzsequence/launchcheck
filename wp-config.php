05a0e715f3e83b4d4e1125cff20c429d447e62eb
Chris Reynolds <chris@jazzsequence.com>
2023-05-05 13:27:48 -0600

    add wp core at root

---
+++ wp-config.php
@@ -0,0 +1,113 @@
+<?php
+/**
+ * The base configuration for WordPress
+ *
+ * The wp-config.php creation script uses this file during the installation.
+ * You don't have to use the web site, you can copy this file to "wp-config.php"
+ * and fill in the values.
+ *
+ * This file contains the following configurations:
+ *
+ * * Database settings
+ * * Secret keys
+ * * Database table prefix
+ * * Localized language
+ * * ABSPATH
+ *
+ * @link https://wordpress.org/support/article/editing-wp-config-php/
+ *
+ * @package WordPress
+ */
+
+// ** Database settings - You can get this info from your web host ** //
+/** This will ensure these are only loaded on Lando */
+if (getenv('LANDO_INFO')) {
+    /**  Parse the LANDO INFO  */
+    $lando_info = json_decode(getenv('LANDO_INFO'));
+
+    /** Get the database config */
+    $database_config = $lando_info->database;
+    /** The name of the database for WordPress */
+    define('DB_NAME', $database_config->creds->database);
+    /** MySQL database username */
+    define('DB_USER', $database_config->creds->user);
+    /** MySQL database password */
+    define('DB_PASSWORD', $database_config->creds->password);
+    /** MySQL hostname */
+    define('DB_HOST', $database_config->internal_connection->host);
+
+    /** URL routing (Optional, may not be necessary) */
+    // define('WP_HOME','http://mysite.lndo.site');
+    // define('WP_SITEURL','http://mysite.lndo.site');
+  };
+
+/** Database charset to use in creating database tables. */
+define( 'DB_CHARSET', 'utf8mb4' );
+
+/** The database collate type. Don't change this if in doubt. */
+define( 'DB_COLLATE', '' );
+
+/**#@+
+ * Authentication unique keys and salts.
+ *
+ * Change these to different unique phrases! You can generate these using
+ * the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}.
+ *
+ * You can change these at any point in time to invalidate all existing cookies.
+ * This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+define( 'AUTH_KEY',          'zz<SFu9[H^-{XV/9jgej*{W7/!eWDz.fm)BWBCYex$DP,ru6hRYN*=40.-fUvjJ:' );
+define( 'SECURE_AUTH_KEY',   'Q8Dc|5Usp6eMNI!Nc8ClD=z+h $~WtYAu%wD.#4$zI(cF)7qDk(=BqBv>P^_-Lbf' );
+define( 'LOGGED_IN_KEY',     '12h^HN9Yt4b:lVEtB6rbBm,$1Igd m[`v0GM7xlZ1{fe,Y67KJ7Em?GK4.`EfT#l' );
+define( 'NONCE_KEY',         'aMwYqtwJ3A]N){q3LsU.-!Q`K~uJ68W0 `Geb&SqfwB+h{`Zs,L,kq0T//)hH/V@' );
+define( 'AUTH_SALT',         'C#0<6lF:a^Q;b_{>eu+6QazxR*^>{Q&O0#8E7)LC3ue/ [_u$,b;nPkGGyQC2ktC' );
+define( 'SECURE_AUTH_SALT',  '%E%(BDxilh[O[%~B,Z1zsq$dY9+b761c2s{cTAXIp+g=p>e1b(<_o)C-@vALANKE' );
+define( 'LOGGED_IN_SALT',    'U_RijQ*,|%{)kf:$T`:?Mb:qH 3dy >Dz]4a%!Wy_3YQz=;UIL~,g>hC}jbci>k*' );
+define( 'NONCE_SALT',        '7x@B>evG7<8CUkK@dgn%~u/7/1uk!MQt; ]C0Pe0lk`EOkC?|1<`yKp^5j&<6*v,' );
+define( 'WP_CACHE_KEY_SALT', '<ksu3Gc!?~mC#XqZekhi:1[g1gKD.NG_zPp.deju{dQGEiKM@07#K[jto]xK;!@6' );
+
+
+/**#@-*/
+
+/**
+ * WordPress database table prefix.
+ *
+ * You can have multiple installations in one database if you give each
+ * a unique prefix. Only numbers, letters, and underscores please!
+ */
+$table_prefix = 'wp_';
+
+
+/* Add any custom values between this line and the "stop editing" line. */
+
+
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ *
+ * For information on other constants that can be used for debugging,
+ * visit the documentation.
+ *
+ * @link https://wordpress.org/support/article/debugging-in-wordpress/
+ */
+if ( ! defined( 'WP_DEBUG' ) ) {
+	define( 'WP_DEBUG', true );
+    define( 'WP_DEBUG_DISPLAY', true );
+    define( 'SCRIPT_DEBUG', true );
+}
+
+/* That's all, stop editing! Happy publishing. */
+
+/** Absolute path to the WordPress directory. */
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', __DIR__ . '/' );
+}
+
+/** Sets up WordPress vars and included files. */
+require_once ABSPATH . 'wp-settings.php';
