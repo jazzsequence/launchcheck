0a4415bcf15179e09ba9bc0dbf3f420d9de6591d
Chris Reynolds <chris@jazzsequence.com>
2023-05-09 13:53:23 -0600

    add multisite support

--- wp-config.php
+++ wp-config.php
@@ -110,6 +110,17 @@ if ( ! defined( 'WP_DEBUG' ) ) {
     define( 'SCRIPT_DEBUG', true );
 }
 
+
+
+define( 'WP_ALLOW_MULTISITE', true );
+define( 'MULTISITE', true );
+define( 'SUBDOMAIN_INSTALL', false );
+$base = '/';
+define( 'DOMAIN_CURRENT_SITE', 'launchcheck.lndo.site' );
+define( 'PATH_CURRENT_SITE', '/' );
+define( 'SITE_ID_CURRENT_SITE', 1 );
+define( 'BLOG_ID_CURRENT_SITE', 1 );
+
 /* That's all, stop editing! Happy publishing. */
 
 /** Absolute path to the WordPress directory. */
