31d12c0c1528f57d7e9bfda7cc3aee906e7c30c5
Chris Reynolds <chris@jazzsequence.com>
2024-04-03 13:16:33 -0600

    update automated tests (#5)
    * update tests to add a mysql server
    
    * check for db host
    
    * fix composer test script
    
    * add a global cmd that allows for gha and local
    
    * define the root pass
    
    * add the host to the script
    
    * move DB_HOST to global env
    
    * need to add the launchcheck:init command so behat is installed
    
    * allow the action to write
    
    * add ssh key for submodule clone
    
    * only test on 7.4
    because that's the version that behat likes
    
    * run a composer update instead of install
    
    * install wp-cli
    
    * exclude the vendor directory
    
    * ignore lots of stuff that fails in wp_launch_check
    
    * fix linting
    
    * run lint before test

--- private/scripts/create-local-db.sh
+++ private/scripts/create-local-db.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
+DB_HOST="${DB_HOST:-localhost}" # Default to localhost if not set
+DB_PASS="${MYSQL_ROOT_PASSWORD:-}"
 
-# Check for the existence of the database
-DB_EXISTS=$(mysql -u root -e "SHOW DATABASES LIKE 'pantheon';" | grep "pantheon" > /dev/null; echo "$?")
+# If DB_PASS is set, define a CMD that includes the password.
+if [ -n "$DB_PASS" ]; then
+    CMD="mysql -h $DB_HOST -u root -p$DB_PASS"
+else
+    CMD="mysql -h $DB_HOST -u root"
+fi
+
+DB_EXISTS=$($CMD -e "SHOW DATABASES LIKE 'pantheon';" | grep "pantheon" > /dev/null; echo "$?")
 
 # If the database does not exist, create it along with the user and grant privileges
-if [ $DB_EXISTS -ne 0 ]; then
-    mysql -u root -e "CREATE DATABASE pantheon; CREATE USER 'pantheon'@'localhost' IDENTIFIED BY 'pantheon'; GRANT ALL PRIVILEGES ON pantheon.* TO 'pantheon'@'localhost'; FLUSH PRIVILEGES;"
+if [ "$DB_EXISTS" -ne 0 ]; then
+    $CMD -e "CREATE DATABASE pantheon; CREATE USER 'pantheon'@'localhost' IDENTIFIED BY 'pantheon'; GRANT ALL PRIVILEGES ON pantheon.* TO 'pantheon'@'localhost'; FLUSH PRIVILEGES;"
 else
     echo "Database pantheon already exists."
 fi
