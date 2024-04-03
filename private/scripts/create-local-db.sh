00ac1a703ded6280f0b66fdd1a7602aacb2db976
Chris Reynolds <chris@jazzsequence.com>
2024-04-03 11:09:06 -0600

    add a script to set up a local db for behat tests

---
+++ private/scripts/create-local-db.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Check for the existence of the database
+DB_EXISTS=$(mysql -u root -e "SHOW DATABASES LIKE 'pantheon';" | grep "pantheon" > /dev/null; echo "$?")
+
+# If the database does not exist, create it along with the user and grant privileges
+if [ $DB_EXISTS -ne 0 ]; then
+    mysql -u root -e "CREATE DATABASE pantheon; CREATE USER 'pantheon'@'localhost' IDENTIFIED BY 'pantheon'; GRANT ALL PRIVILEGES ON pantheon.* TO 'pantheon'@'localhost'; FLUSH PRIVILEGES;"
+else
+    echo "Database pantheon already exists."
+fi
