4e3e6be5d4ca90af1434a69974b500d2e6671697
Chris Reynolds <chris@jazzsequence.com>
2024-04-03 12:21:59 -0600

    add a global cmd that allows for gha and local

--- private/scripts/create-local-db.sh
+++ private/scripts/create-local-db.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 DB_HOST="${DB_HOST:-localhost}" # Default to localhost if not set
+DB_PASS="${MYSQL_ROOT_PASSWORD:-}"
 
+# If DB_PASS is set, define a CMD that includes the password.
+if [ -n "$DB_PASS" ]; then
+    CMD="mysql -u root -p$DB_PASS"
+else
+    CMD="mysql -u root"
+fi
 
-DB_EXISTS=$(mysql -u root -e "SHOW DATABASES LIKE 'pantheon';" | grep "pantheon" > /dev/null; echo "$?")
+DB_EXISTS=$($CMD -e "SHOW DATABASES LIKE 'pantheon';" | grep "pantheon" > /dev/null; echo "$?")
 
 # If the database does not exist, create it along with the user and grant privileges
 if [ $DB_EXISTS -ne 0 ]; then
-    mysql -u root -e "CREATE DATABASE pantheon; CREATE USER 'pantheon'@'localhost' IDENTIFIED BY 'pantheon'; GRANT ALL PRIVILEGES ON pantheon.* TO 'pantheon'@'localhost'; FLUSH PRIVILEGES;"
+    $CMD -e "CREATE DATABASE pantheon; CREATE USER 'pantheon'@'localhost' IDENTIFIED BY 'pantheon'; GRANT ALL PRIVILEGES ON pantheon.* TO 'pantheon'@'localhost'; FLUSH PRIVILEGES;"
 else
     echo "Database pantheon already exists."
 fi
